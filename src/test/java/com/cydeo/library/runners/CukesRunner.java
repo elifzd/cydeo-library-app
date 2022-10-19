7dc4aca00c3835a092654ddddcd055fb42d6c3e3
Elif <elifzulaldemir98@gmail.com>
2022-10-19 10:48:06 +0200

    everything from elif <3 pain

--- src/test/java/com/cydeo/library/runners/CukesRunner.java
+++ src/test/java/com/cydeo/library/runners/CukesRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com/cydeo/library/step_definitions",
-        tags = "@run",
+        tags = "@wip",
         dryRun = false
 )
 
